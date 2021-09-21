079db49066925022c47ccd12709409fe6bd19784
Pedro <revolutionms3@hotmail.com>
2021-09-21 16:13:34 -0300

    Removing electron  and register-scheme dependencies

--- src/util.js
+++ src/util.js
@@ -1,37 +1,29 @@
-'use strict';
+"use strict";
 
 let register;
-try {
-  const { app } = require('electron');
-  register = app.setAsDefaultProtocolClient.bind(app);
-} catch (err) {
-  try {
-    register = require('register-scheme');
-  } catch (e) {} // eslint-disable-line no-empty
-}
 
-if (typeof register !== 'function') {
+if (typeof register !== "function") {
   register = () => false;
 }
 
 function pid() {
-  if (typeof process !== 'undefined') {
+  if (typeof process !== "undefined") {
     return process.pid;
   }
   return null;
 }
 
 const uuid4122 = () => {
-  let uuid = '';
+  let uuid = "";
   for (let i = 0; i < 32; i += 1) {
     if (i === 8 || i === 12 || i === 16 || i === 20) {
-      uuid += '-';
+      uuid += "-";
     }
     let n;
     if (i === 12) {
       n = 4;
     } else {
-      const random = Math.random() * 16 | 0;
+      const random = (Math.random() * 16) | 0;
       if (i === 16) {
         n = (random & 3) | 0;
       } else {
